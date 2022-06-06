330dbf82f065d63340ea885d0f087c64c5f7caf0
Pat Hartl <pat@pathar.tl>
2022-06-06 18:05:09 -0500

    Missing custom logging provider

---
+++ CustomLoggingProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDaemonApps
+{
+    public static class CustomLoggingProvider
+    {
+        public static IHostBuilder UseCustomLogging(this IHostBuilder builder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            return builder.UseSerilog(logger);
+        }
+    }
+}
