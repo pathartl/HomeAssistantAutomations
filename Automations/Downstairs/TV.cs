8c509cb241e02d0b860816b21c387b6a7eee63b0
Pat Hartl <pat@pathar.tl>
2022-06-06 18:04:58 -0500

    Change living room AV receiver to 7ch stereo when music is playing

---
+++ Automations/Downstairs/TV.cs
@@ -0,0 +1,33 @@
+﻿using NetDaemon.HassModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDaemonApps.Automations.Downstairs
+{
+    [NetDaemonApp]
+    public class TV : HomeAssistantAutomation
+    {
+        public TV(IHaContext context, IScheduler scheduler, IMqttEntityManager entityManager, ITextToSpeechService tts) : base(context, scheduler, entityManager, tts)
+        {
+        }
+
+        public override void Init()
+        {
+            Entities.MediaPlayer.LivingRoomTv.StateAllChanges()
+                .Subscribe(s =>
+                {
+                    if (s.Old?.Attributes?.MediaContentType != s.New?.Attributes?.MediaContentType && s.New?.Attributes?.MediaContentType == "music")
+                    {
+                        Services.MediaPlayer.SelectSoundMode(ServiceTarget.FromEntity(Entities.MediaPlayer.Receiver.EntityId), "7ch Stereo");
+                    }
+                    else if (s.Old?.Attributes?.MediaContentType != s.New?.Attributes?.MediaContentType)
+                    {
+                        Services.MediaPlayer.SelectSoundMode(ServiceTarget.FromEntity(Entities.MediaPlayer.Receiver.EntityId), "Surround Decoder");
+                    }
+                });
+        }
+    }
+}
