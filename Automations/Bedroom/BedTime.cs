8d01e95231cf3da1c4ad52fa178e4e8d0efed6a9
Pat Hartl <pat@pathar.tl>
2023-04-21 00:42:52 -0500

    Port bed time routine from HA automation

---
+++ Automations/Bedroom/BedTime.cs
@@ -0,0 +1,45 @@
+﻿using NetDaemon.HassModel.Entities;
+using NetDaemonApps.Extensions;
+using NetDaemonApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetDaemonApps.Automations.Bedroom
+{
+    [NetDaemonApp]
+    public class BedTime : HomeAssistantAutomation
+    {
+        public BedTime(IHaContext context, IScheduler scheduler, IMqttEntityManager entityManager, ITextToSpeechService tts) : base(context, scheduler, entityManager, tts)
+        {
+        }
+
+        public override async void Init()
+        {
+            Entities.MediaPlayer.MasterBathSpeaker.StateAllChanges()
+                .Subscribe(async s =>
+                {
+                    if (s.Entity.Attributes?.AppName == "Relaxing Sounds")
+                    {
+                        Entities.Light.LivingRoomLightsGroup.TurnOff();
+                        Entities.Light.KitchenLightsGroup.TurnOff();
+                        Entities.Light.BedroomLightsGroup.TurnOff();
+                        Entities.Light.EntryLights.TurnOff();
+                        Entities.Switch.OutdoorLights.TurnOff();
+                        Entities.Switch.WallMountedSwitch.TurnOff();
+                        Entities.Light.StairLights.TurnOff();
+                        Entities.Light.OfficeIndicator.TurnOff();
+
+                        Entities.Button.Fan.Press();
+
+                        await Task.Delay(TimeSpan.FromMilliseconds(500));
+
+                        Entities.Button.FanSwing.Press();
+                    }
+                });
+        }
+    }
+}
